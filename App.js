e55190178850ebb7e2d618b77f51d8cbd62fc158
Fábio Salata <fsalata@gmail.com>
2018-09-12 10:51:55 -0300

    Project Setup

---
+++ App.js
@@ -0,0 +1,10 @@
+import { createStackNavigator } from 'react-navigation'
+import Home from './src/screens/Home'
+
+console.disableYellowBox = true
+
+export default createStackNavigator({
+  Home: {
+    screen: Home
+  }
+})
